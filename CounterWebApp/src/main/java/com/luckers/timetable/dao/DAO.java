d70841da11ad30777973b7aaaa52bf8e4ccdc42a
dvorig3 <idvorskij@mail.ru>
2015-04-27 22:14:59 +0300

    Revert "dao"
    This reverts commit e9d26c6aed53267006171295c99f4206a2a18b38.

--- CounterWebApp/src/main/java/com/luckers/timetable/dao/DAO.java
+++ CounterWebApp/src/main/java/com/luckers/timetable/dao/DAO.java
@@ -1,11 +1,7 @@
 package com.luckers.timetable.dao;
 
-import com.luckers.timetable.entities.Week;
-
 /**
  * Created by ihor on 27.04.2015.
  */
 interface  DAO {
-
-   void addWeek(Week week);
 }
--- CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
+++ CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
@@ -1,13 +1,7 @@
 package com.luckers.timetable.dao;
 
-import com.luckers.timetable.entities.Week;
-
 /**
  * Created by ihor on 27.04.2015.
  */
 public class DAOImpl implements DAO {
-    @Override
-    public void addWeek(Week week) {
-
-    }
 }
