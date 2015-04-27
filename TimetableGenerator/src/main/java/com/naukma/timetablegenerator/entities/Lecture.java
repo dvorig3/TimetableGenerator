afb2e588e73b870af4535fbb8085e4f332279563
oliynuk25 <oliynuk25@gmail.com>
2015-04-27 09:58:28 +0300

    dependecy
    hibernate and spring dependecies  was added

---
+++ TimetableGenerator/src/main/java/com/naukma/timetablegenerator/entities/Lecture.java
@@ -0,0 +1,8 @@
+package com.naukma.timetablegenerator.entities;
+
+/**
+ * Created by ihor on 27.04.2015.
+ */
+
+public class Lecture {
+}
--- TimetableGenerator/src/main/java/com/naukma/timetablegenerator/entities/Test.java
+++
@@ -1,8 +0,0 @@
-package com.naukma.timetablegenerator.entities;
-
-/**
- * Created by Администратор on 02.03.2015.
- */
-public class Test {
-
-}
