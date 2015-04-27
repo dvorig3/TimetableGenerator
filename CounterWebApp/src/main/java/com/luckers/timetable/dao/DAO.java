e9d26c6aed53267006171295c99f4206a2a18b38
dvorig3 <idvorskij@mail.ru>
2015-04-27 22:14:34 +0300

    dao

--- CounterWebApp/src/main/java/com/luckers/timetable/dao/DAO.java
+++ CounterWebApp/src/main/java/com/luckers/timetable/dao/DAO.java
@@ -1,7 +1,11 @@
 package com.luckers.timetable.dao;
 
+import com.luckers.timetable.entities.Week;
+
 /**
  * Created by ihor on 27.04.2015.
  */
 interface  DAO {
+
+   void addWeek(Week week);
 }
--- CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
+++ CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
@@ -1,7 +1,13 @@
 package com.luckers.timetable.dao;
 
+import com.luckers.timetable.entities.Week;
+
 /**
  * Created by ihor on 27.04.2015.
  */
 public class DAOImpl implements DAO {
+    @Override
+    public void addWeek(Week week) {
+
+    }
 }
