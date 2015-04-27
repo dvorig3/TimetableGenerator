8debc9fbd3f9401ff555bfde379c72faf587f641
oliynuk25 <oliynuk25@gmail.com>
2015-04-27 22:14:11 +0300

    + annotation

--- CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
+++ CounterWebApp/src/main/java/com/luckers/timetable/dao/DAOImpl.java
@@ -1,7 +1,63 @@
 package com.luckers.timetable.dao;
 
+import com.luckers.timetable.entities.Group;
+import com.luckers.timetable.entities.Lecturer;
+import com.luckers.timetable.entities.Subject;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * Created by ihor on 27.04.2015.
  */
+@Repository
 public class DAOImpl implements DAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void insertGroup(Group group) {
+
+    }
+
+    @Override
+    public void updateGroup(Group group) {
+
+    }
+
+    @Override
+    public Group selectGroupById(int groupId) {
+        return null;
+    }
+
+    @Override
+    public void insertLecturer(Lecturer lecturer) {
+
+    }
+
+    @Override
+    public void updateLecturer(Lecturer lecturer) {
+
+    }
+
+    @Override
+    public Lecturer selectLecturerById(int lecturerId) {
+        return null;
+    }
+
+    @Override
+    public void insertSubject(Subject subject) {
+
+    }
+
+    @Override
+    public void updateSubject(Subject subject) {
+
+    }
+
+    @Override
+    public Subject selectSubjectById(int subjectId) {
+        return null;
+    }
 }
